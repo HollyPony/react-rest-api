eca7cff7c2c95f85e995c0efb0908327e623415c
HollyPony arthur.agombart@gmail.com <arthur.agombart@gmail.com>
2020-03-25 10:13:54 +0100

    v2.1.1 - use no jsx

--- src/Context.js
+++ src/Context.js
@@ -2,6 +2,6 @@ import React, { createContext } from 'react'
 import { buildApi } from './helper'
 
 export const ApiContext = createContext(buildApi())
-export const ApiProvider = ({ children, ...p }) => <ApiContext.Provider value={buildApi(p)}>{children}</ApiContext.Provider>
+export const ApiProvider = ({ children, ...p }) => React.createElement(ApiContext.Provider, { value: buildApi(p) }, children)
 export const ApiConsumer = ApiContext.Consumer
-export const withApi = C => p => <ApiConsumer>{api => <C {...p} api={api} />}</ApiConsumer>
+export const withApi = C => p => React.createElement(ApiConsumer, null, api => React.createElement(C, { ...p, api }))
